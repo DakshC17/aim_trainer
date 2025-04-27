75959993d3e4c035c1bdbf7b949b3b856c9fc1b5
daksh <dakshc1709@gmail.com>
2025-04-28 01:46:05 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import pygame
+import random 
+import math
+import time
+pygame.init()
+
+WIDTH, HEIGHT = 800, 600
+#FPS = 60  #
+
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Aim Trainer")
