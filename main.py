351e0b1302051caee03fe760baefcd33eaec0b54
daksh <dakshc1709@gmail.com>
2025-04-30 00:39:36 +0530

    target collision set

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ BG_COLOR = (0, 10, 40)
 
 class Target:
     MAX_SIZE = 30
-    GROWTH_RATE = 0.5
+    GROWTH_RATE = 0.3
     #GROWTH_RATE = 0.5
     COLOR = "red"
     SECOND_coLOOR = "white"
@@ -54,6 +54,12 @@ class Target:
         pygame.draw.circle(win, self.SECOND_coLOOR, (self.x, self.y), int(self.size*0.4))
 
 
+    def collide(self,x,y): 
+        dis = math.sqrt((self.x-x)**2 + (self.y-y)**2)
+        return dis <= self.size
+
+
+
 def draw(win,targets):
     win.fill(BG_COLOR)  #fill the window with the background color
     for target in targets:
@@ -63,13 +69,23 @@ def draw(win,targets):
 def main():
     run = True
     targets = []   #run is true for now and till it is true the window will run
-    clock = pygame.time.Clock()  #clock is created to control the frame rate
+    clock = pygame.time.Clock()
+    
+    target_pressed = 0
+    clicks = 0
+    misses = 0
+    start_time = time.time()
+    
+      #clock is created to control the frame rate
     pygame.time.set_timer(TARGET_EVENT, TARGET_INCREMENT)  #set timer for target event
 
 
     while run:
 
         clock.tick(60)  #set the frame rate to 60 FPS
+        click = False
+
+        mouse_pos = pygame.mouse.get_pos()  #get the position of the mouse
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -82,9 +98,25 @@ def main():
                 y = random.randint(TARGET_PADDING, HEIGHT - TARGET_PADDING)
                 target = Target(x, y)
                 targets.append(target)
+            
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                click = True
+                clicks += 1
+
 
         for target in targets:
             target.update()
+
+            if target.size <= 0:
+                targets.remove(target)
+                misses += 1
+
+            if click and target.collide(*mouse_pos):
+                
+                targets.remove(target)
+                target_pressed += 1
+
+                
 
         draw(WIN, targets)  #draw the targets on the window
     pygame.quit()
